1bb3e5bf05212650e37edce64134c14b2ce5bc30
Tyler Spear <stylerspear@gmail.com>
2022-10-30 22:04:16 -0500

    Added basic authentication with passport

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/ExpressError')
 const Campground = require('../models/campground')
 const { campgroundSchema } = require('../schemas')
-
+const {isLoggedIn} = require('../middleware')
 const validateCampground = (req, res, next) => {
     const {error} = campgroundSchema.validate(req.body)
     if(error){
@@ -22,12 +22,12 @@ router.get('/', catchAsync(async (req, res) => {
 }))
 
 // ******* SERVE NEW FORM *******
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new')
 })
 
 // ******* NEW FORM ENDPOINT *******
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
     const campground = new Campground(req.body.campground)
     await campground.save()
@@ -47,7 +47,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 }))
 
 // ******* SERVE EDIT FORM *******
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const {id} = req.params
     const campground = await Campground.findById(id)
     if(!campground){
@@ -57,14 +57,14 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const {id} = req.params
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     req.flash('success', 'Successfully updated campground')
     res.redirect(`/campgrounds/${campground._id}`) 
 }))
 
-router.delete('/:id', catchAsync(async(req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async(req, res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id)
     req.flash('success', 'Successfully deleted campground')
